a790e839e9cb22360b3672b3aee6bdbdb7068649
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-03-10 10:55:09 -0300

    feat:ApiCall

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,16 +1,36 @@
+import axios from "axios";
+import { useState, MouseEvent } from "react";
 //Styles
 import { Container, GithubIcon, UserDate, RightArrowIcon } from "./styles";
 
-const SingIn = () => (
-  <Container>
-    <GithubIcon />
-    <UserDate>
-      <input type="text" placeholder="Usuário" required />
-      <button>
-        Entrar
-        <RightArrowIcon />
-      </button>
-    </UserDate>
-  </Container>
-);
+const SingIn = () => {
+  const [gitName, setGitname] = useState("");
+
+  const handleGitname = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    console.log(gitName);
+    axios
+      .get(`https://api.github.com/users/${gitName}`)
+      .then((response) => console.log(response.data));
+  };
+
+  return (
+    <Container>
+      <GithubIcon />
+      <UserDate>
+        <input
+          type="text"
+          placeholder="Usuário"
+          required
+          value={gitName}
+          onChange={(e) => setGitname(e.target.value)}
+        />
+        <button onClick={handleGitname}>
+          Entrar
+          <RightArrowIcon />
+        </button>
+      </UserDate>
+    </Container>
+  );
+};
 export default SingIn;
